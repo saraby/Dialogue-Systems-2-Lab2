d758e137b60ad310d47dce8015d1e44524965779
saraby <sarab.youssef@gmail.com>
2022-09-29 22:57:51 +0200

    Test Speech

--- ddds/http-service/http_service.py
+++ ddds/http-service/http_service.py
@@ -4,6 +4,7 @@ import json
 
 from flask import Flask, request
 from jinja2 import Environment
+import requests
 
 app = Flask(__name__)
 environment = Environment()
@@ -149,3 +150,43 @@ def action_success_response():
         mimetype='application/json'
     )
     return response
+
+
+@app.route("/get_current_data", methods=['POST'])
+def get_current_data(city, country, unit="metric"):
+      
+    API_KEY = "83db75a6c224e10adec4de3c40b7309a"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units={unit}&appid={API_KEY}"
+    print (url)
+    response = requests.get(url)
+    data = response.json()
+    return data
+  
+  
+@app.route("/get_temp", methods=['POST'])
+def get_temp():
+    payload = request.get_json()
+    print("\n"*4, payload, "\n"*4)
+    
+    city = payload["context"]["facts"]["selected_city"]["grammar_entry"]
+    country = payload["context"]["facts"]["selected_country"]["grammar_entry"]
+    if payload["request"]["parameters"]["preferred_unit"]!=None:
+      unit=payload["request"]["parameters"]["preferred_unit"]["grammar_entry"]
+    else:
+      unit = "metric"
+    data = get_current_data(city, country, unit)
+    temperature = str(data['main']['temp'])
+    # unit later
+    return query_response(value=temperature, grammar_entry=None)
+
+
+@app.route("/get_weather", methods=['POST'])
+def get_weather():
+    payload = request.get_json()
+    city = payload["context"]["facts"]["selected_city"]["grammar_entry"]
+    country = payload["context"]["facts"]["selected_country"]["grammar_entry"]
+    data = get_current_data(city, country)
+    the_weather = str(data['weather'][0]['description'])
+    
+    return query_response(value=the_weather, grammar_entry=None)
+
